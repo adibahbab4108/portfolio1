97262bc563ee684756178e8c568efc08ba386320
adibahbab4108 <adibalvi476@gmail.com>
2025-04-18 11:39:55 +0600

    Initial setup done

---
+++ src/assets/certificates/certificates.js
@@ -0,0 +1,15 @@
+import fiveG from "./5g.jpg"
+import cloudToIot from "./cloudtoiot.jpg"
+import cyber1 from "./cyber11.jpg"
+import dataScienceML from "./dataml.jpg"
+import ieee1 from "./ieee1.jpg"
+import webmasterCertificate from "./webmaster.jpg"
+import ph1 from "./programming-hero.jpg"
+import icrcs from "./Md Adib Ahbab.jpg"
+
+export const certificates = {
+    fiveG, cloudToIot,
+    cyber1, dataScienceML,
+    ieee1, webmasterCertificate,
+    ph1, icrcs
+}
